53be80251e830b3321bdfb06e6265d47034e90e4
whittema <shamarsen@mail.ru>
2022-10-31 17:16:09 +0300

    добавил Binding к фрагменту

--- app/src/main/java/com/example/theweather/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/theweather/ui/main/MainFragment.kt
@@ -9,6 +9,8 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import com.example.theweather.R
+import com.example.theweather.databinding.ActivityMainBinding
+import com.example.theweather.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
@@ -17,13 +19,21 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+//    private var _binding: ActivityMainBinding? = null
+//    private val binding get() = _binding!!
+    lateinit var binding : FragmentMainBinding
+
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = FragmentMainBinding.inflate(inflater)
+        return binding.root
     }
+
+
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
