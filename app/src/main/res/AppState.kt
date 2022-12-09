50f32b3f0ccd7cc44ceb8029a94e2d51c85e9dba
whittema <shamarsen@mail.ru>
2022-12-09 13:47:26 +0300

    7 лекция

--- app/src/main/res/AppState.kt
+++
@@ -1,8 +0,0 @@
-import com.example.theweather.domain.Weather
-
-sealed class AppState {
-    data class SuccessOne(val weatherData: Weather) : AppState()
-    data class SuccessMulti(val weatherList: List<Weather>) : AppState()
-    data class Error(val error: Throwable) : AppState()
-    object Loading : AppState()
-}
